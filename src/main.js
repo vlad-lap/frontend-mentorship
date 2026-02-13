c0a8dcb9e62b13b8eeee480f1be05f40f7a2fad0
vlapshov <vlapshov@iponweb.net>
2026-02-13 16:29:49 +0200

    Lesson 3: CSS position + flexbox

--- src/main.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-    let count = 0;
-
-    const button = document.querySelector('button');
-    button.addEventListener('click', () => {
-        count++;
-        console.log(`Button clicked ${count} times`);
-    });
-});
